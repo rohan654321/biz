7e76d499d385166e6550f965a524c40e2aa51d58
Vamshi ch <115910730+vamshichi@users.noreply.github.com>
2025-07-26 12:40:09 +0530

    Removed login for org

--- app/organizer-dashboard/page.tsx
+++ app/organizer-dashboard/page.tsx
@@ -5,10 +5,5 @@ import { authOptions } from "@/lib/auth-options";
 import { redirect } from "next/navigation";
 
 export default async function OrganizerDashboardPage() {
-    const session = await getServerSession(authOptions);
-
-  if (!session || session.user.role !== "organizer") {
-    redirect("/login");
-  }
   return <OrganizerDashboard />
 }
