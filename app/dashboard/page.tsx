6f7545e0271769e806c0d08e150a2796332563c8
Vamshi ch <115910730+vamshichi@users.noreply.github.com>
2025-07-26 12:36:10 +0530

    Removed outh

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,11 +4,5 @@ import { redirect } from "next/navigation"
 import { DashboardLayout } from "@/app/dashboard/dashboard-layout"
 
 export default async function DashboardPage() {
-  const session = await getServerSession(authOptions);
-
-  if (!session || session.user.role !== "admin") {
-    redirect("/login");
-  }
-
   return <DashboardLayout />
 }
