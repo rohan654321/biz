629b3843557b32182e0aab9a8c42ec20ff104668
Sandhyaravoor <sandhyaravoor.25@gmail.com>
2025-10-06 09:57:29 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import "./globals.css"
 import { ClientLayout } from "./client-layout"
+import ConditionalLayout from "./conditional-layout"
 
 export const metadata: Metadata = {
   title: "Biz Trade Fairs",
