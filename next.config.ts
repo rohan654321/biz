a012b8e6c49d3cb208480d92a5fb96613e6c5f4b
Sandeep1816 <avalasandeep02@gmail.com>
2025-12-08 11:31:16 +0530

    tsconfig issue

--- next.config.ts
+++ next.config.ts
@@ -9,47 +9,52 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'sfcc.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "sfcc.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'www.sfcc.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "www.sfcc.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'techexpo2025.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "techexpo2025.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'bfs2025.com', // Add this domain
-        pathname: '/**',
+        protocol: "https",
+        hostname: "bfs2025.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'youtube.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "youtube.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'i.ytimg.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "i.ytimg.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+        pathname: "/**",
       },
     ],
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
